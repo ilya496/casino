0180c68d31467f6191149c5e3a8ae4d36b44366f
Ilja Novak <ilya496>
2025-03-31 22:21:41 +0200

    added dice

--- main.js
+++ main.js
@@ -18,6 +18,11 @@ exrLoader.load('/resources/textures/skybox.exr', (texture) => {
     scene.background = texture;
 });
 
+function placeOnGround(object) {
+    const bbox = new THREE.Box3().setFromObject(object);
+    const minY = bbox.min.y;
+    object.position.y -= minY;
+}
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
@@ -26,19 +31,45 @@ controls.screenSpacePanning = false;
 controls.minDistance = 2;
 controls.maxDistance = 10;
 
+const planeGeometry = new THREE.PlaneGeometry(100, 100);
+const planeMaterial = new THREE.MeshStandardMaterial({ color: 0x808080, side: THREE.DoubleSide });
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.rotation.x = -Math.PI / 2;
+scene.add(plane);
+
 const mtlLoader = new MTLLoader();
-mtlLoader.load('/resources/models/donut.mtl', (materials) => {
-    materials.preload();
-    const objLoader = new OBJLoader();
-    objLoader.setMaterials(materials);
-    objLoader.load('/resources/models/donut.obj', (object) => {
-        scene.add(object);
+
+function loadModel(objFile, mtlFile, position = { x: 0, y: 0, z: 0 }) {
+    mtlLoader.setPath('/resources/models/');
+    mtlLoader.load(mtlFile, (materials) => {
+        materials.preload();
+        
+        const objLoader = new OBJLoader();
+        objLoader.setPath('/resources/models/');
+        objLoader.setMaterials(materials);
+
+        objLoader.load(objFile, (object) => {
+            object.position.set(position.x, position.y, position.z);
+            placeOnGround(object);
+            
+            object.traverse((child) => {
+                if (child.isMesh) {
+                    child.material.side = THREE.DoubleSide;
+                }
+            });
+
+            scene.add(object);
+        }, undefined, (error) => {
+            console.error(`Error loading ${objFile}:`, error);
+        });
     }, undefined, (error) => {
-        console.error(error);
+        console.error(`Error loading ${mtlFile}:`, error);
     });
-}, undefined, (error) => {
-    console.error(error);
-});
+}
+
+// Load models
+loadModel('donut.obj', 'donut.mtl', { x: 0, y: 0, z: 0 });
+loadModel('dice.obj', 'dice.mtl', { x: 2, y: 0, z: 0 });
 
 const light = new THREE.DirectionalLight(0xffffff, 3);
 light.position.set(5, 5, 5);
